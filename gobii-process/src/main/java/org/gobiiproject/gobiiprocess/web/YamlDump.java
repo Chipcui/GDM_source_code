942e7dee149510ccdc4083f2c1bc255a3a2ca525
Phil Glaser <pdg66@cornell.edu>
2016-09-30 15:21:15 -0400

    add yaml dump class

---
+++ gobii-process/src/main/java/org/gobiiproject/gobiiprocess/web/YamlDump.java
@@ -0,0 +1,12 @@
+package org.gobiiproject.gobiiprocess.web;
+
+/**
+ * Created by Phil on 9/30/2016.
+ */
+public class YamlDump {
+
+    public static void main(String[] args) {
+
+        System.err.println("no code");
+    }
+}
