16d411b06cdb38a98eb69f82852d1e4af8c27447
Joshua Lamos-Sweeney <jdl232@cornell.edu>
2017-11-13 16:41:52 -0500

    Lots of little fixes and QOL - now can do multiple different loads without having to restart. Now have a 'clear' button. Now can load a 'reference' file, which will get the name column associated with the column in the viewer. Some refactoring as well, so this functionality (the back end of it, at least) can some day be pulled out into space.

--- TemplateViewer/src/org/gobiiproject/gobiitemplateviewer/TemplateViewer.java
+++ TemplateViewer/src/org/gobiiproject/gobiitemplateviewer/TemplateViewer.java
@@ -14,78 +14,122 @@ import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.ComponentEvent;
-import java.awt.event.ComponentListener;
-import java.io.File;
-import java.io.FileInputStream;
+import java.awt.event.*;
+import java.io.*;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class TemplateViewer implements ActionListener, ComponentListener {
+public class TemplateViewer implements ActionListener, ComponentListener, WindowListener {
 	public File templateFile;
+	public boolean isInstruction;
 	public JFrame mainWindow;
 	public JPanel mainPane;
 	public JScrollPane scrollPane;
+	public File referenceFile;
+	public JMenuItem loadRMenu = new JMenuItem("Load Reference");
+
 
 	public static void main(String[] args) throws InterruptedException {
 		new TemplateViewer();
-		Thread.sleep(100000);
 }
 
 	public TemplateViewer() {
+		mainWindow = new JFrame("Template Viewer");
+		mainWindow.addComponentListener(this);
+		mainWindow.addWindowListener(this);
+		mainWindow.setSize(800, 600);
+
+
+		JMenuBar mainMenu = new JMenuBar();
+		JMenu fileMenu = new JMenu("File");
+		JMenuItem loadMenu = new JMenuItem("Load Template");
+		JMenuItem loadIMenu = new JMenuItem("Load Instruction");
+		JMenuItem clearMenu = new JMenuItem("Clear");
+		loadRMenu.setEnabled(false);//Can't load a reference until you've loaded a template
+
+		loadMenu.addActionListener(this);
+		loadIMenu.addActionListener(this);
+		loadRMenu.addActionListener(this);
+		clearMenu.addActionListener(this);
+		fileMenu.add(loadMenu);
+		fileMenu.add(loadIMenu);
+		fileMenu.add(loadRMenu);
+		mainMenu.add(fileMenu);
+		mainMenu.add(clearMenu);
+		mainWindow.setJMenuBar(mainMenu);
+		mainWindow.repaint();
+		mainWindow.setVisible(true);
+	}
+	private void newMainPanel(){
+		if(mainPane!=null)mainWindow.remove(mainPane);
 		FlowLayout layout = new FlowLayout();
 		mainPane = new JPanel();
 		mainPane.setLayout(layout);
-		scrollPane = new JScrollPane();
-		mainWindow = new JFrame("Template Viewer");
-		mainWindow.addComponentListener(this);
-		mainWindow.setSize(800, 600);
-		scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
-		scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
-		scrollPane.setViewportView(mainPane);
-		JMenuBar mainMenu = new JMenuBar();
-		JMenu fileMenu = new JMenu("File");
-		JMenuItem loadMenu = new JMenuItem("Load");
-		JMenuItem loadIMenu = new JMenuItem("Load Instruction");
-		fileMenu.add(loadMenu);
-		fileMenu.add(loadIMenu);
-		mainMenu.add(fileMenu);
-		mainWindow.setJMenuBar(mainMenu);
-		loadMenu.addActionListener(this);
-		loadIMenu.addActionListener(this);
 		mainWindow.add(mainPane);
-		mainWindow.repaint();
-		mainWindow.setVisible(true);
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		if (e.getActionCommand().equals("Load") || e.getActionCommand().equals("Load Instruction")) {
+		if (e.getActionCommand().equals("Load Template") || e.getActionCommand().equals("Load Instruction")) {
+			newMainPanel();//Start a new 'main' panel
 			JFileChooser chooser = new JFileChooser();
 			FileNameExtensionFilter f = new FileNameExtensionFilter(".json", "json");
 			chooser.addChoosableFileFilter(f);
 			chooser.setFileFilter(f);
 			chooser.showOpenDialog(mainWindow);
+
+			//Set up template file and instruction variables
 			this.templateFile = chooser.getSelectedFile();
-			System.out.println(templateFile.toString());
-			boolean loadInstruction = e.getActionCommand().equals("Load Instruction");
-			List<GobiiLoaderInstruction> instructions;
-			if (loadInstruction) {
-				instructions = HelperFunctions.parseInstructionFile(templateFile.toString());
-			} else {
-				instructions = getFromJobFile(templateFile.toString());
+			isInstruction = e.getActionCommand().equals("Load Instruction");
+
+			loadFile();
+			if(templateFile!=null) {
+				loadRMenu.setEnabled(true);
 			}
-			mainPane.setLayout(new GridLayout((instructions.size() + 1) / 2, 2));
-			mainWindow.setName(templateFile.getName());
-			for (GobiiLoaderInstruction instruction : instructions) {
-				System.out.println(instruction.getTable());
-				//mainPane.add(new JTextField(instruction.getTable()));
-				mainPane.add(new Table(instruction).panel);
-			}
-			mainWindow.repaint();
+		}
+
+		else if(e.getActionCommand().equals("Load Reference")){
+			JFileChooser chooser = new JFileChooser();
+			chooser.showOpenDialog(mainWindow);
+			this.referenceFile = chooser.getSelectedFile();
+
+			//Redo the load and display, this time with a reference set
+			newMainPanel();
+			loadFile();
+		}
+		else if(e.getActionCommand().equals("Clear")){
+			templateFile=null; referenceFile=null;
+			newMainPanel();
 			mainWindow.setVisible(true);
+		}
+	}
+
+	/**
+	 * Assuming everything's set up correctly (isInstruction, mainPane, etc, this method places data into them based on
+	 * the loaded isntruction file.
+	 */
+	private void loadFile() {
+		if(templateFile==null)return;
+		List<GobiiLoaderInstruction> instructions = getInstructions();
+		System.out.println("Instruction length " + instructions.size());
+		mainPane.setLayout(new GridLayout((instructions.size() + 1) / 2, 2));
+		mainWindow.setName(templateFile.getName());
+		for (GobiiLoaderInstruction instruction : instructions) {
+			mainPane.add(new Table(instruction).panel);
+		}
+
+		//Repaint main window
+		mainWindow.repaint();
+		mainWindow.setVisible(true);//causes appropriate refresh? though mainWindow.repaint doesn't. I hate swing.
+	}
+
+	//Using templateFile and isInstruction, reads either a template or an instruction file, returning the GLI list
+	private List<GobiiLoaderInstruction> getInstructions() {
+		if (isInstruction) {
+			return HelperFunctions.parseInstructionFile(templateFile.toString());
+		} else {
+			return getFromJobFile(templateFile.toString());
 		}
 	}
 
@@ -136,6 +180,41 @@ public class TemplateViewer implements ActionListener, ComponentListener {
 		return null;
 	}
 
+	@Override
+	public void windowOpened(WindowEvent e) {
+
+	}
+
+	@Override
+	public void windowClosing(WindowEvent e) {
+
+	}
+
+	@Override
+	public void windowClosed(WindowEvent e) {
+		System.exit(0);
+	}
+
+	@Override
+	public void windowIconified(WindowEvent e) {
+
+	}
+
+	@Override
+	public void windowDeiconified(WindowEvent e) {
+
+	}
+
+	@Override
+	public void windowActivated(WindowEvent e) {
+
+	}
+
+	@Override
+	public void windowDeactivated(WindowEvent e) {
+
+	}
+
 	class Table {
 		public JPanel panel;
 
@@ -171,25 +250,36 @@ public class TemplateViewer implements ActionListener, ComponentListener {
 					content.add(new JSeparator(JSeparator.HORIZONTAL));//Add a vertical separator
 				}
 				content.add(new JLabel(c.getName()));
-				if(c.getGobiiColumnType().equals(GobiiColumnType.CSV_ROW)){
-					content.add(new JLabel("Column " + c.getcCoord()));
-
-				}
-				else if(c.getGobiiColumnType().equals(GobiiColumnType.CSV_COLUMN)){
-					content.add(new JLabel("Row "  + c.getrCoord()));
-				}
-				else if(c.getGobiiColumnType().equals(GobiiColumnType.CSV_BOTH)) {
-					content.add(new JLabel("" + c.getrCoord() + " : " + c.getcCoord()));
-				}
-				else if(c.getGobiiColumnType().equals(GobiiColumnType.CONSTANT)){
-					content.add(new JLabel("Always "+c.getConstantValue()));
-				}
-				else if(c.getGobiiColumnType().equals(GobiiColumnType.AUTOINCREMENT)){
-					content.add(new JLabel("Increments Automatically"));
-				}
-				else{
-					content.add(new JLabel("Type "+c.getGobiiColumnType()));
-				}
+				String valueLabel;
+				switch(c.getGobiiColumnType()) {
+					case CSV_ROW:
+						if (referenceFile != null) {
+							valueLabel=readCSV_ROW(referenceFile,inst,c.getrCoord(),c.getcCoord());
+						} else {
+							valueLabel = "Column " + c.getcCoord();
+						}
+						break;
+					case CSV_COLUMN:
+						if (referenceFile != null) {
+							valueLabel=readCSV_ROW(referenceFile,inst,c.getrCoord()-1,c.getcCoord());
+						} else {
+							valueLabel = "Row " + c.getrCoord();
+						}
+						break;
+					case CSV_BOTH:
+						valueLabel="" + c.getrCoord() + " : " + c.getcCoord();
+						break;
+					case CONSTANT:
+						valueLabel="Always " + c.getConstantValue();
+						break;
+					case AUTOINCREMENT:
+						valueLabel="Increments Automatically";
+						break;
+						default:
+							valueLabel="Type " + c.getGobiiColumnType();
+						break;
+					}
+				content.add(new JLabel(valueLabel));
 				if (c.getFilterFrom() != null || c.getFilterTo()!=null) {
 					if(c.getFilterFrom() == null){
 						content.add(new JLabel("Filter to " + c.getFilterTo()));
@@ -204,7 +294,26 @@ public class TemplateViewer implements ActionListener, ComponentListener {
 			}
 			content.setBackground(backgroundColor);
 			panel.add(content);
-			//border.addLayoutComponent(content,BorderLayout.SOUTH);
 		}
+	}
+
+	//Stolen and reporpoised from CSVFileReaderV2
+	private static String readCSV_ROW(File file, GobiiLoaderInstruction loaderInstruction,int row, int col){
+		try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
+			int rowNo = 0;
+			String fileRow;
+			while ((fileRow = bufferedReader.readLine()) != null) {
+				if (rowNo >= row) {
+					// All required rows read.
+					break;
+				}
+				rowNo++;
+			}
+				String[] rowVal = fileRow.split(loaderInstruction.getGobiiFile().getDelimiter());
+			return rowVal[col];
+		}catch (FileNotFoundException e) {
+		} catch (IOException e) {
+		}
+		return "ERROR";
 	}
 }
